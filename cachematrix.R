d9cd6fcb6a122a54c4bf32b91d1d1b16de9a6b5f
ammhd <a.m.m.hasan@ed.ac.uk>
2017-05-26 15:45:21 +0100

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function takes a matrix from the user and the cacheSolve function calculates the inverse of it if there's not a inverse matrix already calculated or provided separately.
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix function sets a matrix, which can be pursed by get object. Sets a inverse matrix to null if already not calculated. And, that even can be pursed with getivrs element.After calling the matrix from the makeCacheMatrix, the cacheSolve function calculates the inverse matrix if the getinvrs element is still null. Otherwise, get the inverse matrix from cache.
+
+
+## makeCacheMatrix function sets a matrix, which can be pursed by get object. Sets a inverse matrix to null if already not calculated. And, that even can be pursed with getivrs element.
+
 
 makeCacheMatrix <- function(x = matrix()) {
+my_invrs <- NULL
+  set <- function(y) {
+    x <<- y
+    my_invrs <<- NULL
+  }
+  get <- function() x
+  setinvrs <- function(inverse) my_invrs <<- inverse
+  getinvrs <- function() my_invrs
+  list(set = set, get = get,
+       setinvrs = setinvrs,
+       getinvrs = getinvrs)
 
 }
 
 
-## Write a short comment describing this function
+## After calling the matrix from the makeCacheMatrix, the cacheSolve function calculates the inverse matrix if the getinvrs element is still null. Otherwise, get the inverse matrix from cache.n
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  my_invrs <- x$getinvrs()
+  if(!is.null(my_invrs)) {
+    message("getting cached data")
+    return(my_invrs)
+  }
+  data <- x$get()
+  my_invrs <- solve(data, ...)
+  x$setinvrs(my_invrs)
+  my_invrs
+
 }
